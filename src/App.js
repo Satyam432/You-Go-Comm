80043c632196179e50b1c03e904bf4854b9995e6
Satej Bidvai <satej.bidvai@gmail.com>
2020-07-25 15:49:48 +0530

    Sets up the initial folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Events from './components/Events';
+import Footer from './components/Footer';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<Router>
+			<Navbar />
+			<Switch>
+				<Route path='/events' component={Events} />
+				<Route path='/' exact component={Home} />
+			</Switch>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
