6854663f27c3ccfbc42db8744fec8e08f27e4ec1
grv1 <gauravdotiya@gmail.com>
2020-08-06 05:50:41 +0530

    friends

--- server/models/db.js
+++ server/models/db.js
@@ -1,6 +1,4 @@
 const Sequelize = require('sequelize');
-// const faker = require('faker');
-// const Promise = require('bluebird');
 
 const db = {};
 
@@ -13,27 +11,20 @@ db.User = require('./auth/User');
 db.Event = require('./Event');
 
 // Add Relations/ Associations
-db.User.belongsToMany(db.Event, {
-  through: 'user_event',
+db.User.belongsToMany(db.User, {
+  as: 'Follower',
+  through: 'friends',
+  foreignKey: 'follower_id',
   onDelete: 'CASCADE',
   onUpdate: 'CASCADE',
 });
 
-// Promise.all([
-//   db.User.create({
-//     name: faker.name.findName(),
-//     email: faker.internet.email(),
-//     college: 'BITS',
-//     state: faker.address.state(),
-//     city: faker.address.city(),
-//     degree: 'B.Tech',
-//     dob: faker.date.past(),
-//     contact: faker.phone.phoneNumber(),
-//     image_url: faker.internet.url(),
-//   }),
-//   db.Event.create({
-//     name: faker.commerce.productName(),
-//   }),
-// ]);
+db.User.belongsToMany(db.User, {
+  as: 'Following',
+  through: 'friends',
+  foreignKey: 'following_id',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE',
+});
 
 module.exports = db;
